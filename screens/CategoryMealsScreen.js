53e74cd3c23891836ec58c28cd3bddc25fcb2f07
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-05 13:39:22 +0800

    changed the header for the chosen category's meals Category Meals Screen by setting up a function and finding the title

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,6 +1,7 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 import React from "react";
 
 // Display the different meals for the selected cuisine from the Cuisine aka Category Grid
@@ -15,11 +16,16 @@ import React from "react";
 
 // find takes a function and executes on every element in the array
 
+// The selected category based on two lines below is availble in the CategoryMealsScreen Component and not outside it
+// const catID = props.navigation.getParam("categoryId");
+// const selectedCategory = CATEGORIES.find((cat) => cat.id === catID);
+
 const CategoryMealsScreen = (props) => {
   const catID = props.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catID);
   return (
     <View style={styles.screen}>
+      <Text>The CategoryMealsScreen</Text>
       <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meals"
@@ -35,6 +41,19 @@ const CategoryMealsScreen = (props) => {
       />
     </View>
   );
+};
+
+// JS object or Function since we have changing data for the selected category
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  catID = navigationData.navigation.getParam("categoryId");
+  selectedCategory = CATEGORIES.find((cat) => cat.id === catID);
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
 };
 
 export default CategoryMealsScreen;
