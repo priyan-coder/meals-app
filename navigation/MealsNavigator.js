1092a1a380225b0f72f75bd03d7485e2dd75c7bb
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-05 16:46:26 +0800

    app works

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -10,6 +10,11 @@
 // Instead of specifiying the navigationOptions at each screen after declaring its component
 // Adding it under MealsNavigator is better
 
+// Pass the defaultNavigationOptions aka default settings as a second param of createStackNavigator
+// Those options applied to every screen in the Navigator
+// The navigationOptions specified in the various screens take precedence or can override the default settings applied here
+// Hit control + space to check for other options to set in the second param for createStackNavigator
+
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import Colors from "../constants/Colors";
@@ -18,28 +23,27 @@ import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const MealsNavigator = createStackNavigator({
-  Categories: {
-    screen: CategoriesScreen,
-    navigationOptions: {
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen,
+    },
+    MealDetail: MealDetailScreen,
+  },
+
+  {
+    initialRouteName: "Categories",
+    defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
       },
       headerTintColor:
         Platform.OS === "android" ? "white" : Colors.primaryColor,
     },
-  },
-  CategoryMeals: {
-    screen: CategoryMealsScreen,
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
-      },
-      headerTintColor:
-        Platform.OS === "android" ? "white" : Colors.primaryColor,
-    },
-  },
-  MealDetail: MealDetailScreen,
-});
+  }
+);
 
 export default createAppContainer(MealsNavigator);
